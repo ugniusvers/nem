4b4cc11e9a3a8d485f4074e0539429b64abc576e
ugve4191 <ugnius.versekenas@mif.stud.vu.lt>
2019-08-07 12:54:38 +0300

    openAnAccountWallet

--- source/scripts/shell/openAnAccountWallet.sh
+++ source/scripts/shell/openAnAccountWallet.sh
@@ -1,2 +1,12 @@
+
 #!/bin/bash
-nem2-cli profile create --privatekey ../../generatedAddresses/privateKey.txt --network MIJIN_TEST --url http://localhost:3000 --profile wallet
+
+privateKey=`cat ../../generatedAddresses/privateKey.txt`
+nem2-cli profile create --privatekey $privateKey --network MIJIN_TEST --url http://localhost:3000 --profile test1
+
+curl=`curl http://localhost:3000/block/1`
+genHash=${curl%%totalFee*}
+genHash=${genHash##*generationHash}
+genHash=${genHash:3}
+genHash=${genHash::-3}
+echo $genHash>>../../generatedAddresses/generationHash.txt
